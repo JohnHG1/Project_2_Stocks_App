4d8dda5f233f781df93a62ead7ab0a0bbc7df4c4
JohnHG1 <honisz@yahoo.com>
2020-03-07 12:12:13 +0000

    StockService.js added

---
+++ client/src/services/StockService.js
@@ -0,0 +1,27 @@
+const baseURL = 'http://localhost:3000/api/stocks/'
+
+export default {
+  getStocks(){
+    return fetch(baseURL)
+    .then(res => res.json())
+  },
+
+  postStock(payload){
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { 'Content-type': 'application/json'}
+    })
+    .then(res => res.json())
+  },
+  deleteStock(id) {
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    })
+  },
+  updateStock(id) {
+    return fetch(baseURL + id, {
+      method: 'PATCH'
+    })
+  }
+}
