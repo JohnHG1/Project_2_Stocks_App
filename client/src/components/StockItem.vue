130c1f8aba7b4f3c4c9307ec1071fe6f2f1f157d
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-03-09 17:18:19 +0000

    adds new stock number and price to stockItem

--- client/src/components/StockItem.vue
+++ client/src/components/StockItem.vue
@@ -1,7 +1,11 @@
 <template lang="html">
   <div class="stock-item">
-    <span>{{stock['stock_symbol']}}</span>
-    <span>£{{parseFloat(stock['number_of_shares']*stock['price']).toFixed(2)}}</span>
+    <div class="item_line_1">
+      <span>{{stock['stock_symbol']}}</span>
+      <span>£{{parseFloat(stock['number_of_shares']*stock['price']).toFixed(2)}}</span>
+      <p>{{stock.number_of_shares}} @ {{stock.price}}</p>
+    </div>
+
   </div>
 </template>
 
@@ -15,9 +19,15 @@ export default {
 <style lang="css" scoped>
 .stock-item {
 display: flex;
-justify-content: space-around;
+justify-content: space-between;
 font-size: 1.5em;
 border-bottom: 1px solid black;
 padding: 2% 0;
 }
+
+p{
+  text-align: right;
+  font-size: 1em;
+}
+
 </style>
